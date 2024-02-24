1cc2c1d31f5a33ec82ff50d68857a2e2009a4828
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-24 20:13:40 +0530

    add bgChanger with random bg generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
+
+
+
+
 
 function App() {
+  const [color, setcolor] = useState("olive");
 
+
+  const generateColor = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const hexColor = "#" + randomColor;
+  
+    setcolor(hexColor)
+  }
 
   return (
-    <>
-    <h1 className="bg-slate-100 p-2">Hello world</h1>
-    </>
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+      <div>{color}</div>
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-md">
+          <button className="outline-none px-4" onClick={() => setcolor("Red")}>Red</button>
+          <button className="outline-none px-4" onClick={() => setcolor("Yellow")}>Yellow</button>
+          <button className="outline-none px-4" onClick={() => setcolor("Green")}>Green</button>
+          <button className="outline-none px-4" onClick={() => setcolor("Blue")}>Blue</button>
+          <button className="outline-none px-4" onClick={() => setcolor("Orange")}>Orange</button>
+          <button className="outline-none px-4" onClick={generateColor}>Random </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
